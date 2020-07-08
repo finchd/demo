df8ea25c6adbca83c6c902231bd4ca5aa888baa4
Donovan Finch <finchd@users.noreply.github.com>
2020-07-07 18:55:08 -0700

    Add [pre-commit](pre-commit.com) w/ Black applied

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='helloworld',
+    name="helloworld",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'flask',
-    ],
-    setup_requires=[
-        'pytest-runner',
-    ],
-    tests_require=[
-        'pytest',
-    ],
+    install_requires=["flask",],
+    setup_requires=["pytest-runner",],
+    tests_require=["pytest",],
 )
